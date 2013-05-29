e21bed350f8c54be62c85fd08f288a885a4b619a
Greg Leppert <greg@formasfunction.com>
2013-05-29 08:05:36 -0400

    Adding missing Video props

--- lib/tuml/posts/video_post.rb
+++ lib/tuml/posts/video_post.rb
@@ -1,28 +1,33 @@
 class Tuml
   class VideoPost < Post
 
-    HEIGHT_REGEXP = /height="(\d+)"/
-
     # Rendered for Video posts.
     block 'Video' do
       true
     end
 
     # Rendered if there is a caption for this post.
-    block 'Caption' do
-      find('Caption') != ''
-    end
+    block 'Caption'
 
     # The caption for this post.
     tag 'Caption' do
       data['caption']
     end
 
-    [500, 400, 250].each do |width|
+    [250,400,500,700].each do |width|
       tag "Video-#{width}" do
         data['player'].find {|p| p['width'] == width}['embed_code']
       end
+      tag "VideoEmbed-#{width}" do
+        data['player'].find {|p| p['width'] == width}['embed_code']
+      end
     end
+
+    tag "PlayCount"
+
+    tag "FormattedPlayCount"
+
+    tag "PlayCountWithLabel"
 
   end
 end
